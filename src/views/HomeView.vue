e43b984b3b79108cf5582fd48acd996a71281d94
Tseing <im.yczeng@foxmail.com>
2025-04-05 19:03:44 +0800

    feat: add DailyCards and MarqueeCards components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,53 +30,18 @@
         class="absolute h-[304vh] w-[120vw] -translate-x-[10vw] -translate-y-[20vh] rotate-6 bg-[url(./assets/bg-map.webp)] bg-cover bg-center opacity-50 brightness-110 saturate-40"
       ></div>
       <div class="relative mt-12 h-screen bg-white/10">
-        <img class="absolute h-1/3" src="../assets/water-color.svg" />
-        <div class="relative flex flex-col px-6 py-12 md:px-16 md:py-16">
-          <div class="flex items-baseline space-x-8">
-            <h2 class="font-slogan text-6xl text-zinc-800 md:text-8xl">
-              每日一词
-            </h2>
-            <h3
-              class="font-slogan text-4xl text-red-500 md:text-5xl"
-              style="text-shadow: 4px -4px 0 skyblue"
-            >
-              4月1日
-            </h3>
-          </div>
-          <div
-            class="px:12 flex flex-col space-y-4 py-16 md:space-y-8 lg:px-32"
-          >
-            <div>
-              <ruby
-                class="text-5xl font-bold break-all whitespace-normal text-zinc-800 md:text-6xl lg:text-7xl"
-                style="ruby-align: center"
-              >
-                文<rp>(</rp><rt>ma<sup>21</sup></rt
-                ><rp>)</rp>字<rp>(</rp><rt>lang<sup>21</sup></rt
-                ><rp>)</rp>文<rp>(</rp><rt>sung<sup>53</sup></rt
-                ><rp>)</rp>字<rp>(</rp><rt>ma<sup>21</sup></rt
-                ><rp>)</rp>文<rp>(</rp><rt>lang<sup>21</sup></rt
-                ><rp>)</rp>字<rp>(</rp><rt>sung<sup>53</sup></rt
-                ><rp>)</rp>
-              </ruby>
-            </div>
-            <p
-              class="line-ellipsis text-xl leading-8 font-medium text-ellipsis text-zinc-800 md:text-2xl md:leading-10 lg:text-3xl lg:leading-12"
-            >
-              ○解释解释解释支斥給哭菜具戶眼乍足品假苗泉訴弓皮。○旦房占歡草豆冒首您林公中亭五屋手良書民葉；清丟足犬課麻弓止跑欠甲石尺胡見耍，做聽什怎穴貓者，地長就助林她山喜戶和用。
-            </p>
-            <button
-              type="button"
-              class="me-2 mb-2 flex w-fit flex-row items-center space-x-3 rounded-lg bg-blue-700 px-8 py-2 font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              <span>查阅详情</span
-              ><span class="material-symbols-rounded">arrow_right_alt</span>
-            </button>
-          </div>
-        </div>
+        <DailyCards></DailyCards>
       </div>
       <div class="relative h-screen border-t-2 border-t-red-500">
-        <div class="pt-56"></div>
+        <div class="flex flex-col items-center space-y-8 pt-20">
+          <h2 class="font-slogan text-7xl text-zinc-800">
+            送十主诉里汗朵良四肖再弓石犬示英
+          </h2>
+          <h3 class="font-slogan text-5xl">
+            牙菜天步一旁住南河哥，直植爪北放巾
+          </h3>
+        </div>
+        <div class="pt-40"><MarqueeCards></MarqueeCards></div>
       </div>
     </div>
     <div class="relative z-10 -mt-32 h-56 rounded-t-[48px] bg-zinc-700"></div>
@@ -89,6 +54,8 @@ import { vOnClickOutside } from '@vueuse/components';
 import CircleAnimation from '../components/CircleAnimation.vue';
 import NavBar from '../components/NavBar.vue';
 import SearchBar from '../components/SearchBar.vue';
+import MarqueeCards from '../components/MarqueeCards.vue';
+import DailyCards from '../components/DailyCards.vue';
 
 const isHistoryVisible = ref(false);
 
@@ -118,12 +85,4 @@ rt {
   font-size: xx-large;
   color: var(--color-sky-800);
 }
-
-/* .line-ellipsis {
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  -webkit-line-clamp: 2;
-} */
 </style>
