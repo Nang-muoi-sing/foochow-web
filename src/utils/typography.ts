19d121bdf94790b09e97e11c39e97d6a8cc37f81
Tseing <im.yczeng@foxmail.com>
2025-07-04 22:51:34 +0800

    refactor: move pronunciation ruby helper to typography.ts

---
+++ src/utils/typography.ts
@@ -0,0 +1,25 @@
+export const makeYngpingRubyInner = (
+  text: string,
+  yngping: string,
+  rubyClass: string = ''
+): string => {
+  const chars = text.trim().split('');
+  const charProns = yngping.trim().split(' ');
+  //  TODO: <sub>
+
+  if (charProns.length !== chars.length) {
+    return `<span class="rb">${text}</span><rp>(</rp><rt class="${rubyClass}">${makeYngpingSup(yngping)}</rt><rp>)</rp>`;
+  } else {
+    let rubyString = '';
+    for (let i = 0; i < chars.length; i++) {
+      rubyString += `<span class="rb">${chars[i]}</span><rp>(</rp><rt class="${rubyClass}">${makeYngpingSup(charProns[i])}</rt><rp>)</rp>`;
+    }
+    return rubyString;
+  }
+};
+
+export const makeYngpingSup = (yngping: string): string => {
+  const tone = yngping.match(/\d+$/);
+  const yngpingLetter = yngping.replace(/\d+$/, '');
+  return tone ? `${yngpingLetter}<sup>${tone}</sup>` : yngping;
+};
