c2d13058d0839abd08627d53b2fb40fe7a28c5c3
Tseing <im.yczeng@foxmail.com>
2025-07-04 22:52:31 +0800

    feat: add PageContent as page layout

---
+++ src/components/PageContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-wheat-50 h-full min-h-[100vh] w-full">
+    <NavBar></NavBar>
+
+    <div
+      class="bg-whear-100 px-96 py-5"
+      style="min-height: calc(100vh - 130px - var(--spacing) * 56)"
+    >
+      <!-- 内联样式暂时没有优雅的方法 -->
+      <slot></slot>
+    </div>
+    <Footer class="relative bottom-0 w-full"></Footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from '../components/NavBar.vue';
+import Footer from '../components/Footer.vue';
+</script>
