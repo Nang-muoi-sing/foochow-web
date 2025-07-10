291175385c4ce1e1bb458e9573262863294c423d
Tseing <im.yczeng@foxmail.com>
2025-07-10 21:37:00 +0800

    refactor: simplify DOM nodes of SeeSymbol and add icon for CikLing annotations

--- src/components/WordCikLingCard.vue
+++ src/components/WordCikLingCard.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    v-if="props.data?.length > 0"
+    v-if="processedData?.length > 0"
     class="mt-2 mb-5 rounded-lg bg-white px-8 py-6"
   >
     <div class="mb-2 space-y-2">
       <WordCikLingEntry
-        v-for="(cikling, index) in props.data"
+        v-for="(cikling, index) in processedData"
         :data="cikling"
         :key="index"
       ></WordCikLingEntry>
     </div>
     <div class="text-rosybrown-800 space-y-1">
-      <template v-for="(cikling, index) in props.data" :key="index"
+      <template v-for="(cikling, index) in processedData" :key="index"
         ><p>
           <Badge
             v-if="cikling.liAnnotateCikOrder"
@@ -37,7 +37,7 @@
     <template v-if="props.isCommentedCikLing">
       <hr class="border-rosybrown-100 my-2 border-t-2" />
       <div class="text-rosybrown-800 space-y-1">
-        <template v-for="(cikling, index) in props.data" :key="index">
+        <template v-for="(cikling, index) in processedData" :key="index">
           <SeeSymbol
             v-if="cikling.comment"
             icon="注释"
@@ -51,14 +51,36 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { toneCikLingMap } from '../utils/model';
 import type { WordCikLing } from '../utils/typing';
 import Badge from './Badge.vue';
 import SeeSymbol from './SeeSymbol.vue';
 import WordCikLingEntry from './WordCikLingEntry.vue';
-import { toneCikLingMap } from '../utils/model';
 
 const props = defineProps<{
   data: WordCikLing[];
   isCommentedCikLing: boolean;
 }>();
+
+const processedData = computed(() => {
+  return props.data.map((item) => {
+    const tone = toneCikLingMap[item.tone];
+    const cikAnnotation =
+      item.cikAnnotation == '' ? '' : removeNumber(item.cikAnnotation);
+    const lingAnnotation =
+      item.lingAnnotation == '' ? '' : removeNumber(item.lingAnnotation);
+
+    return {
+      ...item,
+      tone,
+      cikAnnotation,
+      lingAnnotation,
+    };
+  });
+});
+
+const removeNumber = (str: string) => {
+  return str.replace(/\d+$/, '').trim();
+};
 </script>
