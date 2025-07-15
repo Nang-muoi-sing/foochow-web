bbcc047e6b260566976a94e759c2dd0e69214cf1
Tseing <im.yczeng@foxmail.com>
2025-07-15 23:00:32 +0800

    feat: #10 为 WordSkeleton 和 SearchSkeleton 添加响应式

--- src/components/WordSkeleton.vue
+++ src/components/WordSkeleton.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="bg-wheat-100 mt-5 mb-8 rounded-lg px-8 py-6">
     <div class="animate-pulse space-y-3.5">
-      <div class="bg-wheat-200 mt-4.5 h-6 w-40 rounded-lg"></div>
-      <div class="bg-wheat-200 mb-4.5 h-6 w-28 rounded-lg"></div>
+      <div class="bg-wheat-200 mt-4.5 h-6 w-[50%] rounded-lg sm:w-40"></div>
+      <div class="bg-wheat-200 mb-4.5 h-6 w-[30%] rounded-lg sm:w-28"></div>
     </div>
   </div>
-  <div class="bg-wheat-100 mt-4.5 mb-4 h-6 w-32 animate-pulse rounded-lg"></div>
+  <div
+    class="bg-wheat-100 mt-4.5 mb-4 h-6 w-[35%] animate-pulse rounded-lg sm:w-32"
+  ></div>
   <div class="mt-2 mb-5 rounded-lg bg-white px-8 py-6">
     <div class="my-3 animate-pulse">
-      <div class="bg-wheat-200 mb-4 h-6 w-40 rounded-lg"></div>
-      <div class="bg-wheat-100 mt-6 mb-2.5 h-3 w-115 rounded-lg"></div>
-      <div class="bg-wheat-100 mb-2.5 h-3 w-75 rounded-lg"></div>
-      <div class="bg-wheat-100 mb-2.5 h-3 w-90 rounded-lg"></div>
-      <div class="bg-wheat-100 mb-2.5 h-3 w-56 rounded-lg"></div>
+      <div class="bg-wheat-200 mb-4 h-6 w-[35%] rounded-lg sm:w-40"></div>
+      <div
+        class="bg-wheat-100 mt-6 mb-2.5 h-3 w-[95%] rounded-lg sm:w-95 md:w-115"
+      ></div>
+      <div class="bg-wheat-100 mb-2.5 h-3 w-[75%] rounded-lg sm:w-75"></div>
+      <div class="bg-wheat-100 mb-2.5 h-3 w-[90%] rounded-lg sm:w-90"></div>
+      <div class="bg-wheat-100 mb-2.5 h-3 w-[56%] rounded-lg sm:w-56"></div>
     </div>
   </div>
-  <div class="bg-wheat-100 mt-4.5 mb-4 h-6 w-32 animate-pulse rounded-lg"></div>
+  <div
+    class="bg-wheat-100 mt-4.5 mb-4 h-6 w-[35%] animate-pulse rounded-lg sm:w-32"
+  ></div>
   <div class="mt-2 mb-5 rounded-lg bg-white px-8 py-6">
     <div class="my-3 animate-pulse">
-      <div class="bg-wheat-200 mb-4 h-6 w-40 rounded-lg"></div>
-      <div class="bg-wheat-100 mt-6 mb-2.5 h-3 w-115 rounded-lg"></div>
-      <div class="bg-wheat-100 mb-2.5 h-3 w-75 rounded-lg"></div>
-      <div class="bg-wheat-100 mb-2.5 h-3 w-90 rounded-lg"></div>
-      <div class="bg-wheat-100 mb-2.5 h-3 w-56 rounded-lg"></div>
+      <div class="bg-wheat-200 mb-4 h-6 w-[35%] rounded-lg sm:w-40"></div>
+      <div
+        class="bg-wheat-100 mt-6 mb-2.5 h-3 w-[95%] rounded-lg sm:w-95 md:w-115"
+      ></div>
+      <div class="bg-wheat-100 mb-2.5 h-3 w-[75%] rounded-lg sm:w-75"></div>
+      <div class="bg-wheat-100 mb-2.5 h-3 w-[90%] rounded-lg sm:w-90"></div>
+      <div class="bg-wheat-100 mb-2.5 h-3 w-[56%] rounded-lg sm:w-56"></div>
     </div>
   </div>
 </template>
