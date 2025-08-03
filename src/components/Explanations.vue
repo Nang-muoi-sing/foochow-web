e2887f2d7a83fca2d2af6197e300a8a5e2e35367
Tseing <im.yczeng@foxmail.com>
2025-08-04 00:16:57 +0800

    fix: #4 榕拼中无声调时在 IPA 中视作 0 调

--- src/components/Explanations.vue
+++ src/components/Explanations.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps } from 'vue';
+import { computed } from 'vue';
 import { replaceChineseQuotes, toggleGlyph } from '../utils/typography';
 import type { FengExplNode } from '../utils/typing';
 
