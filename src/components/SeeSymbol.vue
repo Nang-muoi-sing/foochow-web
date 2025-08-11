893962e164c180a3d5275857f30dd1c56b914c46
Paul Sun <econsunrq@outlook.com>
2025-08-11 12:56:05 +0800

    fix: #9 解决解释样式重叠

--- src/components/SeeSymbol.vue
+++ src/components/SeeSymbol.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="seeicon- mr-1 align-text-bottom" style="font-size: 20px"
+  <span class="seeicon- mr-1 align-text-bottom" style="font-size: 20px; display: inline-block; width: 1em;"
     ><slot></slot
   ></span>
 </template>
