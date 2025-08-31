21bb2b6c4b01a12e609d708f87bd17460bb2c448
Tseing <im.yczeng@foxmail.com>
2025-08-31 10:44:50 +0800

    feat: 添加 ICP 备案信息

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="bg-rosybrown-800 h-56 rounded-t-[48px] text-center">
-    <div class="pt-20">
-      <span class="text-sans text-rosybrown-50">© 2025 米时典 SeeDict </span>
+    <div class="flex flex-col pt-20">
+      <span class="text-sans text-rosybrown-50 mb-2"
+        >© 2025 米时典 SeeDict
+      </span>
+      <div v-if="showBeian" class="text-rosybrown-400">
+        <a href="beian.miit.gov.cn" target="_blank"
+          >闽 ICP 备 2025111999 号 - 1</a
+        >
+      </div>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const showBeian = import.meta.env.VITE_ENV_NAME !== 'github-ci';
+</script>
